9a8c09cd5b145f803789312698e1afe89c7ee84a
Ismail Rasheed (IT) <ismail.r@macl.aero>
2022-04-06 10:26:39 +0500

    init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function login(){
+        return view('auth.login');
+    }
+}
